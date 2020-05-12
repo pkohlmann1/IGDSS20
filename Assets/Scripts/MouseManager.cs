31282743336164f442fd614b554457c6bd56768c
Philip Kohlmann <philip_kohlmann@yahoo.de>
2020-05-12 20:55:47 +0200

    Added CameraController

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -15,4 +15,16 @@ public class MouseManager : MonoBehaviour
     {
         
     }
+
+    void GetMouseButtonClicks()
+    {
+        if (Input.GetMouseButtonDown(0))
+            Debug.Log("Pressed primary button.");
+
+        if (Input.GetMouseButtonDown(1))
+            Debug.Log("Pressed secondary button.");
+
+        if (Input.GetMouseButtonDown(2))
+            Debug.Log("Pressed middle click.");
+    }
 }
