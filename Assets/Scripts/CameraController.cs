f328ade3da0c27ddc54f99dcfb57475f73b0c49b
Philip Kohlmann <philip_kohlmann@yahoo.de>
2020-05-16 14:26:05 +0200

    CameraScript Kommentiert

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -36,8 +36,9 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        minX = -0.5f * GM.HeightMap.width * GM.TileRadius*GM.HexDisplace;
-        maxX = 0.5f * GM.HeightMap.width * GM.TileRadius*GM.HexDisplace;
+        //Set initial values
+        minX = -0.5f * GM.HeightMap.width * GM.TileRadius * GM.HexDisplace;
+        maxX = 0.5f * GM.HeightMap.width * GM.TileRadius * GM.HexDisplace;
         minZ = -0.5f * GM.HeightMap.height * GM.TileRadius;
         maxZ = 0.5f * GM.HeightMap.height * GM.TileRadius;
         newPosition = transform.position;
@@ -54,11 +55,13 @@ public class CameraController : MonoBehaviour
 
     void HandleMouseInput()
     {
-        if(Input.mouseScrollDelta.y != 0)
+        // Scrollen mit Mausrad
+        if (Input.mouseScrollDelta.y != 0)
         {
             newZoom += Input.mouseScrollDelta.y * zoomAmount;
         }
 
+        //Mit linker Maustaste Kamera bewegen
         if (Input.GetMouseButtonDown(0))
         {
             Plane plane = new Plane(Vector3.up, Vector3.zero);
@@ -72,7 +75,7 @@ public class CameraController : MonoBehaviour
                 dragStartPosition = ray.GetPoint(entry);
             }
         }
-
+        //Mit linker Maustatse Kamera bewegen
         if (Input.GetMouseButton(0))
         {
             Plane plane = new Plane(Vector3.up, Vector3.zero);
@@ -96,11 +99,12 @@ public class CameraController : MonoBehaviour
             }
         }
 
+        // Mit rechter Maustatse Kamera rotieren
         if (Input.GetMouseButtonDown(1))
         {
             rotateStartPosition = Input.mousePosition;
         }
-        if(Input.GetMouseButton(1))
+        if (Input.GetMouseButton(1))
         {
             rotateCurrentPosition = Input.mousePosition;
 
@@ -109,12 +113,13 @@ public class CameraController : MonoBehaviour
             rotateStartPosition = rotateCurrentPosition;
 
             newRotation *= Quaternion.Euler(Vector3.up * (-difference.x / 5f));
-           //newRotation *= Quaternion.Euler(1 * (difference.y / 5f), 1 * (-difference.x / 5f), 0);
+            //newRotation *= Quaternion.Euler(1 * (difference.y / 5f), 1 * (-difference.x / 5f), 0);
         }
     }
 
     void HandleMovementInput()
     {
+        //Bei gedrückter Shift-Taste mit höherer Geschwindigkeit bewegen
         if (Input.GetKey(KeyCode.LeftShift))
         {
             movementSpeed = fastSpeed;
@@ -123,23 +128,29 @@ public class CameraController : MonoBehaviour
         {
             movementSpeed = normalSpeed;
         }
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+
+        // Bei gedrückter W-Taste Kamera nach vorne bewegen
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             newPosition += (transform.forward * movementSpeed);
         }
+        // Bei gedrückter S-Taste Kamera nach hinten bewegen
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             newPosition += (transform.forward * -movementSpeed);
         }
+        // Bei gedrückter D-Taste Kamera nach rechts bewegen
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             newPosition += (transform.right * movementSpeed);
         }
+        // Bei gedrückter A-Taste Kamera nach links bewegen
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             newPosition += (transform.right * -movementSpeed);
         }
 
+        // Mit Q und E-Taste Kamera rotieren
         if (Input.GetKey(KeyCode.Q))
         {
             newRotation *= Quaternion.Euler(Vector3.up * rotationAmount);
@@ -149,6 +160,7 @@ public class CameraController : MonoBehaviour
             newRotation *= Quaternion.Euler(Vector3.up * -rotationAmount);
         }
 
+        // Mit R und F-Taste Kamera rein und rauszoomen
         if (Input.GetKey(KeyCode.R))
         {
             newZoom += zoomAmount;
@@ -158,12 +170,12 @@ public class CameraController : MonoBehaviour
             newZoom -= zoomAmount;
         }
 
+        // Zoom und Kamerasteuerung limitieren, sodass Karte nicht verlassen werden kann
         newZoom.y = Mathf.Clamp(newZoom.y, minY, maxY);
-
         newPosition.x = Mathf.Clamp(newPosition.x, minX, maxX);
         newPosition.z = Mathf.Clamp(newPosition.z, minZ, maxZ);
 
-
+        //Kameraposition -rotation je nach Input aktualisieren
         transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
         transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * movementTime);
         cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * movementTime);
