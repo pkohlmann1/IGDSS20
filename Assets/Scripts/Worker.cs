8abd342d09afda586b4e61b345b225a46591e625
Philip Kohlmann <philip.kohlmann@yahoo.de>
2020-06-19 16:45:57 +0200

    Worker Prefab + Component + FarmersResidence

--- Assets/Scripts/Worker.cs
+++ Assets/Scripts/Worker.cs
@@ -6,21 +6,28 @@ public class Worker : MonoBehaviour
 {
     #region Manager References
     JobManager _jobManager; //Reference to the JobManager
-    GameManager _gameManager;//Reference to the GameManager
+    public GameManager _gameManager;//Reference to the GameManager
     #endregion
 
     public float _age; // The age of this worker
     public float _happiness; // The happiness of this worker
 
+    public float _fishConsumption;
+    public float _clothConsumption;
+    public float _schnappsConsumtpion;
+
     // Start is called before the first frame update
     void Start()
     {
+        _gameManager = GameObject.FindWithTag("GameManager").GetComponent<GameManager>();
+        InvokeRepeating("Age", 15.0f, 15.0f);
+        InvokeRepeating("´ConsumeRessources", 60.0f, 60.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Age();
+       
     }
 
 
@@ -29,6 +36,8 @@ public class Worker : MonoBehaviour
         //TODO: Implement a life cycle, where a Worker ages by 1 year every 15 real seconds.
         //When becoming of age, the worker enters the job market, and leaves it when retiring.
         //Eventually, the worker dies and leaves an empty space in his home. His Job occupation is also freed up.
+
+       _age += 1;
 
         if (_age > 14)
         {
@@ -60,5 +69,12 @@ public class Worker : MonoBehaviour
     private void Die()
     {
         Destroy(this.gameObject, 1f);
+    }
+
+    private void ConsumeRessources()
+    {
+        _gameManager._resourcesInWarehouse[GameManager.ResourceTypes.Fish] -= _fishConsumption;
+        _gameManager._resourcesInWarehouse[GameManager.ResourceTypes.Clothes] -= _clothConsumption;
+        _gameManager._resourcesInWarehouse[GameManager.ResourceTypes.Schnapps] -= _schnappsConsumtpion;
     }
 }
